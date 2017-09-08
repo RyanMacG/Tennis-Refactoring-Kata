63e698b6fc89a6b340219f7b15f9443a7b0175af
Ryan MacGillivray <rmacg89@gmail.com>
2017-09-08 15:17:37 +0100

    Keep strings in score method
    Reduce complexity of win_for and advantage_for

--- python/tennis.py
+++ python/tennis.py
@@ -75,22 +75,23 @@ class TennisGame2:
                 P1res="Thirty"
             result = P1res + "-" + P2res
 
+        if self.win_for(self.p1points, self.p2points):
+            return "Win for " + self.player1Name
+        if self.win_for(self.p2points, self.p1points):
+            return "Win for " + self.player2Name
+        if self.advantage_for(self.p1points, self.p2points):
+            return "Advantage " + self.player1Name
+        if self.advantage_for(self.p2points, self.p1points):
+            return "Advantage " + self.player2Name
 
-        result = self.advantage_for(self.p1points, self.p2points, self.player1Name, result)
-        result = self.advantage_for(self.p2points, self.p1points, self.player2Name, result)
-        result = self.win_for(self.p1points, self.p2points, self.player1Name, result)
-        result = self.win_for(self.p2points, self.p1points, self.player2Name, result)
         return result
 
-    def win_for(self, player_points, other_player_points, player_name, result):
-        if (player_points >= 4 and other_player_points >= 0 and (player_points-other_player_points) >= 2):
-            result = "Win for " + player_name
-        return result
 
-    def advantage_for(self, player_points, other_player_points, player_name, result):
-        if (player_points > other_player_points and other_player_points >= 3):
-            result = "Advantage " + player_name
-        return result
+    def win_for(self, player_points, other_player_points):
+        return player_points >= 4 and other_player_points >= 0 and (player_points-other_player_points) >= 2
+
+    def advantage_for(self, player_points, other_player_points):
+        return player_points > other_player_points and other_player_points >= 3
 
     def SetP1Score(self, number):
         for i in range(number):
